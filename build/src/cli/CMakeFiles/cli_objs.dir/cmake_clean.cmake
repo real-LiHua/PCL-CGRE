157f22e4a3026fdf56ddd980a8f0a077e5e4cf3f
x-leehe <78580595+x-leehe@users.noreply.github.com>
2026-06-19 15:36:24 +0800

    chore: Deleted build directory

--- build/src/cli/CMakeFiles/cli_objs.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/cli_objs.dir/CliCommands.cpp.o"
-  "CMakeFiles/cli_objs.dir/CliCommands.cpp.o.d"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/cli_objs.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/src/cli/CMakeFiles/cli_objs.dir/placeholder.cpp.o.d
+++
@@ -1,3 +0,0 @@
-src/cli/CMakeFiles/cli_objs.dir/placeholder.cpp.o: \
- /home/admin/Devel/PCL-GTK/src/cli/placeholder.cpp \
- /usr/include/stdc-predef.h
